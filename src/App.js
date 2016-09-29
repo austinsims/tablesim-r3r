fd3c0be430887200b828d18caa9548e2b37f471e
Austin Sims <adsims2001@gmail.com>
2016-09-28 22:49:21 -0400

    gots a cube!

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {Simple} from './Simple';
+
 class App extends Component {
   render() {
     return (
@@ -11,7 +13,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {<Simple />}
         </p>
       </div>
     );
